153a8d4656817a9f9c87521bf250fedc12490d5f
the-technical-quant-trader <luvratan.techengineer@gmail.com>
2025-11-17 12:26:26 +0530

    email service done

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -89,7 +89,7 @@ export async function POST(req) {
     // ✅ Send the email
     await transporter.sendMail({
       from: `"Decipher Insights" <${process.env.EMAIL_USER}>`,
-      to: ["luv.ratan@decipherfinancials.com", "megha.punjabi@decipherfinancials.com","isha.joshi@decpipherfinancials.com"],
+      to: ["luv.ratan@decipherfinancials.com", "megha.punjabi@decipherfinancials.com","isha.joshi@decipherfinancials.com", "anil.kumar@decipherfinancials.com", "sneha.mehta@decipherfinancials.com"],
       subject: "Form Submission - Approval Required",
       html: emailHtml,
     });
